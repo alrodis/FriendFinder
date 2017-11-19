aa99f502bb7e6233c62fc538056fc5fe56153276
Alvin Rodis <alrodis147@gmail.com>
2017-11-19 15:16:48 -0600

    11-19 commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,19 @@
 //pulling in data we want to display on api route
 var resultsArray = require("../data/friends.js");
 
+//ROUTING
 //GET Route to display JSON of all possible friends
 module.exports = function (app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(resultsArray);
 	});
-}
+
 
 //POST Route to handle incoming survey results.  This route also handles the compatibility logic
+	app.post("/api/friends", function(req, res){
+		var receivedResults = req.body;
+		console.log(receivedResults);//console logging in terminal, grabbng user input data from survey
+		resultsArray.push(req.body);
+		res.json(receivedResults);
+	});
+};
