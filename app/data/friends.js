fd38f8c2ac8b443477b37afce696cb0f9b0b787a
Alvin Rodis <alrodis147@gmail.com>
2017-11-19 19:05:34 -0600

    11-19 commit_2

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,14 @@
 //DATA: results of Survey
 var resultsArray = [
 	{
-		"name": "Alvin",
+		"name": "Alvin Rodis",
 		"photo": "https://github.com/alrodis",
 		"scores": [5,1,2,4,5,2,3,5,1,3]
+	},
+	{
+		"name": "Russell Westbrook",
+		"photo": "http://www.billboard.com/files/styles/article_main_image/public/media/Russell-Westbrook-2017-smiling-billboard-1548.jpg",
+		"scores": [4,3,1,1,5,4,2,5,2,3]
 	}
 ];
 
